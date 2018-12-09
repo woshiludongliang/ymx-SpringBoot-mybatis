46323140fd1a8d61a73c27714d5dc7664de44484
dongliang <1367340440@qq.com>
2018-12-09 15:52:18 +0800

    mybatis-plus简单分页的使用

---
+++ ymx/ymx-dao/src/main/java/com/dong/liang/db/entity/User.java
@@ -0,0 +1,47 @@
+package com.dong.liang.db.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author dl
+ * @since 2018-12-09
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    private Integer sex;
+
+
+}
